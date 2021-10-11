08eff0d1a57e7429931e6228874a3ba930dbedfc
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2021-10-11 19:58:04 -0400

    Remove ignored files

---
+++ src/main/java/rad/mining/ethermine/EthermineApplication.java
@@ -0,0 +1,13 @@
+package rad.mining.ethermine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthermineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthermineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rad/mining/ethermine/EthermineApplicationTests.java
@@ -0,0 +1,13 @@
+package rad.mining.ethermine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthermineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
