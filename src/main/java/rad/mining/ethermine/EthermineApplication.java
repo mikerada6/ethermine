c384fb8c77171dc7715719edb21117665fcd7d2e
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2022-02-23 21:56:46 -0500

    add EthermineApplication

--- src/main/java/rad/mining/ethermine/EthermineApplication.java
+++ src/main/java/rad/mining/ethermine/EthermineApplication.java
@@ -2,14 +2,23 @@ package rad.mining.ethermine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class EthermineApplication {
+public class EthermineApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EthermineApplication.class, args);
+	public static
+	void main(String[] args) {
+		SpringApplication.run(EthermineApplication.class,
+				args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EthermineApplication.class);
 	}
 
 }
